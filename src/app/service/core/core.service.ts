9db6227e1e3e394826ca6e4c73e6a797dd46171e
Gustavo Santamaria <gustavo.santamaria@rappi.com>
2020-08-04 09:23:27 -0500

    mock services for tasks and users

--- src/app/service/core/core.service.ts
+++ src/app/service/core/core.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
 import {TaskModel} from '../../_model';
+import {DaysEnum} from '../../_enum';
+import {UserModel} from '../../_model/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -39,6 +41,109 @@ export class CoreService {
             name: 'Nestor',
           }
         ]
+      }
+    ];
+  }
+
+  public async listTasks(): Promise<Array<TaskModel>> {
+    await setTimeout(null, 1000);
+
+    return [
+      {
+        name: 'Cooking',
+        schedule: ['12:30 P.M.'],
+        users: [
+          {
+            id: 'uVn3URVzCKXYd1BB84GACilUNet1',
+            name: 'Gustavo',
+          },
+          {
+            id: 'B84GACilUNet1uVn3URVzCKXYd1B',
+            name: 'Rachel',
+          },
+          {
+            id: 'B84GACKXYd1BCilUNet1uVn3URVz',
+            name: 'Nestor',
+          }
+        ],
+        days: [
+          DaysEnum.MON,
+          DaysEnum.TUE,
+          DaysEnum.WEN,
+          DaysEnum.THU,
+          DaysEnum.FRI,
+          DaysEnum.SAT,
+          DaysEnum.SUN
+        ]
+      },
+      {
+        name: 'Do the dishes',
+        schedule: ['7:30 P.M.'],
+        users: [
+          {
+            id: 'uVn3URVzCKXYd1BB84GACilUNet1',
+            name: 'Gustavo',
+          },
+          {
+            id: 'B84GACilUNet1uVn3URVzCKXYd1B',
+            name: 'Rachel',
+          },
+          {
+            id: 'B84GACKXYd1BCilUNet1uVn3URVz',
+            name: 'Nestor',
+          }
+        ],
+        days: [
+          DaysEnum.MON,
+          DaysEnum.TUE,
+          DaysEnum.WEN,
+          DaysEnum.THU,
+          DaysEnum.FRI,
+          DaysEnum.SAT,
+          DaysEnum.SUN
+        ]
+      },
+      {
+        name: 'Take out the trash',
+        schedule: ['8:00 P.M.'],
+        users: [
+          {
+            id: 'uVn3URVzCKXYd1BB84GACilUNet1',
+            name: 'Gustavo',
+          },
+          {
+            id: 'B84GACilUNet1uVn3URVzCKXYd1B',
+            name: 'Rachel',
+          },
+          {
+            id: 'B84GACKXYd1BCilUNet1uVn3URVz',
+            name: 'Nestor',
+          }
+        ],
+        days: [
+          DaysEnum.MON,
+          DaysEnum.WEN,
+          DaysEnum.FRI
+        ]
+      }
+    ];
+  }
+
+  public async listUsers(): Promise<Array<UserModel>> {
+    await setTimeout(null, 1000);
+
+    return [
+      {
+        id: 'uVn3URVzCKXYd1BB84GACilUNet1',
+        name: 'Gustavo',
+      },
+      {
+        id: 'B84GACilUNet1uVn3URVzCKXYd1B',
+        name: 'Rachel',
+      },
+      {
+        id: 'B84GACKXYd1BCilUNet1uVn3URVz',
+        name: 'Nestor',
       }
     ];
   }
